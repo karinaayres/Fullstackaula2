aa13714f2f1aeb20a19ae040e507b1d8f722e49b
Karina Ayres <karinaayres@hotmail.com>
2025-08-29 21:23:39 -0300

    Implementação do repositorio

---
+++ Alunosolution/Aluno.Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+        void IAlunoRepositorio.Adicionar()
+        {
+            throw new NotImplementedException();
+        }
+
+        List<int> IAlunoRepositorio.Listar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Alunosolution/Aluno.Repositorio/IAlunoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using Aluno.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aluno.Repositorio
+{
+    internal interface IAlunoRepositorio
+    {
+        void Adicionar();
+        List<int> Listar();
+    }
+
+   
+}
